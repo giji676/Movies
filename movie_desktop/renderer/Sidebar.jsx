1c559d0162283fc2cdd48b8fdf0b89190de5f89a
giji676 <tvtengoxachidze@gmail.com>
2025-09-01 23:25:01 +0100

    better sidebar ui

--- movie_desktop/renderer/Sidebar.jsx
+++ movie_desktop/renderer/Sidebar.jsx
@@ -1,14 +1,41 @@
 import { NavLink } from 'react-router-dom';
+import { FaRegBookmark, FaHistory, FaCog, FaDesktop} from "react-icons/fa";
 import styles from './Sidebar.module.css';
+
 
 function Sidebar({ resetMovieListData }) {
     return (
         <div className={styles.sidebar}>
             <h2 className={styles.logo}>🎬</h2>
-            <NavLink to="/" onClick={() => {resetMovieListData()}} className={styles.button}>Dis</NavLink>
-            <NavLink to="/history" className={styles.button}>His</NavLink>
-            <NavLink to="/watch-later" className={styles.button}>Wat</NavLink>
-            <NavLink to="/settings" className={styles.button}>Set</NavLink>
+            <NavLink 
+                to="/" 
+                onClick={() => {resetMovieListData()}} 
+                className={({ isActive }) =>
+                    `${styles.button} ${isActive ? styles.active : ''}`
+                }>
+                <FaDesktop className={styles.btnIcon}/>
+            </NavLink>
+            <NavLink 
+                to="/history" 
+                className={({ isActive }) =>
+                    `${styles.button} ${isActive ? styles.active : ''}`
+                }>
+                    <FaHistory className={styles.btnIcon}/>
+            </NavLink>
+            <NavLink 
+                to="/watch-later" 
+                className={({ isActive }) =>
+                    `${styles.button} ${isActive ? styles.active : ''}`
+                }>
+                    <FaRegBookmark className={styles.btnIcon}/>
+            </NavLink>
+            <NavLink 
+                to="/settings" 
+                className={({ isActive }) =>
+                    `${styles.button} ${isActive ? styles.active : ''}`
+                }>
+                    <FaCog className={styles.btnIcon}/>
+            </NavLink>
         </div>
     );
 }
