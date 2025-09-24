a7dffaa1f6fc2dcf069de6177e5598448af1b906
giji676 <tvtengoxachidze@gmail.com>
2025-09-24 15:34:23 +0100

    setup ci/cd pipeline

--- docker/backend/wsgi-entrypoint.sh
+++ docker/backend/wsgi-entrypoint.sh
@@ -1,28 +1,21 @@
 #!/bin/sh
 
-until cd /app/backend/
-do
+echo "Starting backend entrypoint..."
+
+# Wait for static volume
+until [ -d /app/backend/movie/static/ ]; do
     echo "Waiting for movie volume..."
+    sleep 1
 done
 
-until ./manage.py migrate
-do
-    echo "Waiting for db to be ready..."
+# Wait for DB & run migrations
+until python manage.py migrate; do
+    echo "Waiting for DB to be ready..."
     sleep 2
 done
 
-./manage.py collectstatic --noinput
+# Collect static files
+python manage.py collectstatic --noinput
 
-gunicorn movie.wsgi:application --bind 0.0.0.0:8000 --workers 4 --threads 4
-
-#####################################################################################
-# Options to DEBUG Django server
-# Optional commands to replace abouve gunicorn command
-
-# Option 1:
-# run gunicorn with debug log level
-# gunicorn server.wsgi --bind 0.0.0.0:8000 --workers 1 --threads 1 --log-level debug
-
-# Option 2:
-# run development server
-# DEBUG=True ./manage.py runserver 0.0.0.0:8000
+# Start gunicorn
+exec gunicorn movie.wsgi:application --bind 0.0.0.0:8000 --workers 4 --threads 4
