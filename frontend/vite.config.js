31320b9cecf0f7ae8f53448b60883e0c5c6b52f0
giji676 <tvtengoxachidze@gmail.com>
2025-09-29 21:37:56 +0100

    frontend proxy to backend

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
     root: 'renderer',
     plugins: [react()],
     envDir: path.resolve(__dirname), // <-- project root
@@ -12,5 +12,19 @@ export default defineConfig({
     },
     server: {
         host: true, // needed for LAN access
+        ...(mode === 'dev' && {
+            proxy: {
+                '/api': {
+                    target: 'http://localhost:8000',
+                    changeOrigin: true,
+                    secure: false,
+                },
+                '/media/downloads': {
+                    target: 'http://localhost:8000',
+                    changeOrigin: true,
+                    secure: false,
+                }
+            }
+        })
     },
-});
+}));
