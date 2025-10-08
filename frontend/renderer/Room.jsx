f28e0471ab8583f11c3e6be1528ea1c55fa4459a
giji676 <tvtengoxachidze@gmail.com>
2025-10-08 11:49:58 +0100

    progress tracking/sync

--- frontend/renderer/Room.jsx
+++ frontend/renderer/Room.jsx
@@ -24,22 +24,6 @@ function Room() {
     const [isLoading, setIsLoading] = useState(false);
     const [isPlaying, setIsPlaying] = useState(false);
 
-    const setUpWebSocket = () => {
-        const socket = new WebSocket(url);
-        socketRef.current = socket;
-
-        socket.onmessage = (e) => {
-            let data = JSON.parse(e.data);
-            if (data.action === "playing") {
-                setIsPlaying(data.action_state);
-            }
-        }
-
-        return () => {
-            socket.close();
-        };
-    };
-
     useEffect(() => {
         setUpWebSocket();
         setMovieId(room.movie_id);
@@ -48,91 +32,6 @@ function Room() {
     useEffect(() => {
         getMoviePath();
     }, [movieId]);
-
-    const getMoviePath = async () => {
-        if (isLoading || !movieId) return;
-        try {
-            setIsLoading(true);
-            const res = await api.get(`/movie/stream-to-client/?tmdb_id=${movieId}`);
-            const data = res.data;
-            if (data.file_path) {
-                //setVideoPath(data.file_path);
-                setVideoPath("http://localhost:5173/media/downloads/22/hls/Pirates_of_the_Caribbean__The_Curse_of_the_Black_Pearl.m3u8");
-            } else {
-                toast.error("No file_path returned for movie:", tmdb_id);
-            }
-        } catch (err) {
-            toast.error("Failed to fetch movie path", err);
-        } finally {
-            setIsLoading(false);
-        }
-    };
-
-    useEffect(() => {
-        if (socketRef.current?.readyState !== WebSocket.OPEN) return;
-        console.log("update - isPlaying:", isPlaying);
-        socketRef.current.send(JSON.stringify({
-            "type": "room_action",
-            "action": "playing",
-            "action_state": isPlaying,
-        }));
-    }, [isPlaying]);
-
-    const updateTimeStamp = async () => {
-        if (!videoRef.current) return;
-
-        const currentTime = Math.floor(videoRef.current.currentTime); // seconds
-        try {
-            await api.patch(`/playlist/${tmdb_id}/update-progress/`, { time_stamp: currentTime });
-        } catch (error) {
-            toast.error("Failed to update time stamp:", error.response?.data || error.message);
-        }
-    };
-
-    const startTracking = () => {
-        if (updateInterval.current) return;
-        updateInterval.current = setInterval(updateTimeStamp, 15000); // every 15 seconds
-    };
-
-    const stopTracking = () => {
-        if (updateInterval.current) {
-            clearInterval(updateInterval.current);
-            updateInterval.current = null;
-        }
-    };
-
-    const updateState = () => {
-        const video = videoRef.current;
-        if (!video) return;
-
-        setIsPlaying(!video.paused);
-    };
-
-    const setupTrackingListeners = () => {
-        const video = videoRef.current;
-        if (!video) return;
-
-        const onSeek = () => {
-            // updateTimeStamp();
-        };
-
-        video.addEventListener("play", updateState);
-        video.addEventListener("pause", updateState);
-        video.addEventListener("ended", stopTracking);
-        video.addEventListener("seeked", onSeek);
-
-        if (!videoRef.current.paused) {
-            startTracking();
-        }
-
-        return () => {
-            stopTracking();
-            video.removeEventListener("play", updateState);
-            video.removeEventListener("pause", stopTracking);
-            video.removeEventListener("ended", stopTracking);
-            video.removeEventListener("seeked", onSeek);
-        };
-    };
 
     useEffect(() => {
         if (!videoPath || !videoRef.current ) return;
@@ -165,8 +64,110 @@ function Room() {
         return cleanup;
     }, [videoPath]);
 
+    useEffect(() => {
+        if (socketRef.current?.readyState !== WebSocket.OPEN) return;
+        socketRef.current.send(JSON.stringify({
+            "type": "room_action",
+            "action": "playing",
+            "action_state": isPlaying,
+        }));
+    }, [isPlaying]);
+
+    const setUpWebSocket = () => {
+        const socket = new WebSocket(url);
+        socketRef.current = socket;
+
+        socket.onmessage = (e) => {
+            let data = JSON.parse(e.data);
+            if (data.action === "playing") {
+                setIsPlaying(data.action_state);
+            }
+        }
+
+        return () => {
+            socket.close();
+        };
+    };
+
+    const getMoviePath = async () => {
+        if (isLoading || !movieId) return;
+        try {
+            setIsLoading(true);
+            const res = await api.get(`/movie/stream-to-client/?tmdb_id=${movieId}`);
+            const data = res.data;
+            if (data.file_path) {
+                //setVideoPath(data.file_path);
+                setVideoPath("http://localhost:5173/media/downloads/22/hls/Pirates_of_the_Caribbean__The_Curse_of_the_Black_Pearl.m3u8");
+            } else {
+                toast.error("No file_path returned for movie:", tmdb_id);
+            }
+        } catch (err) {
+            toast.error("Failed to fetch movie path", err);
+        } finally {
+            setIsLoading(false);
+        }
+    };
+
+    const updateTimeStamp = async () => {
+        if (!videoRef.current) return;
+        if (socketRef.current?.readyState !== WebSocket.OPEN) return;
+
+        const currentTime = Math.floor(videoRef.current.currentTime); // seconds
+        console.log("updating time:", currentTime);
+
+        socketRef.current.send(JSON.stringify({
+            "type": "room_action",
+            "action": "sync",
+            "action_state": currentTime,
+        }));
+    };
+
+    const startTracking = () => {
+        if (updateInterval.current) return;
+        updateInterval.current = setInterval(updateTimeStamp, 15000); // every 15 seconds
+    };
+
+    const stopTracking = () => {
+        if (updateInterval.current) {
+            clearInterval(updateInterval.current);
+            updateInterval.current = null;
+        }
+    };
+
+    const updateState = () => {
+        const video = videoRef.current;
+        if (!video) return;
+
+        setIsPlaying(!video.paused);
+    };
+
+    const setupTrackingListeners = () => {
+        const video = videoRef.current;
+        if (!video) return;
+
+        const onSeek = () => {
+            updateTimeStamp();
+        };
+
+        video.addEventListener("play", () => {updateState(); startTracking();});
+        video.addEventListener("pause", () => {updateState(); stopTracking();});
+        video.addEventListener("ended", stopTracking);
+        video.addEventListener("seeked", onSeek);
+
+        if (!videoRef.current.paused) {
+            startTracking();
+        }
+
+        return () => {
+            stopTracking();
+            video.removeEventListener("play", stopTracking);
+            video.removeEventListener("pause", stopTracking);
+            video.removeEventListener("ended", stopTracking);
+            video.removeEventListener("seeked", onSeek);
+        };
+    };
+
     const togglePlay = () => {
-        console.log("toggle play");
         if (!videoRef.current) return;
         if (videoRef.current.paused) {
             videoRef.current.play().catch(() => toast.warn("Autoplay blocked"));
