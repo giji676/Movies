321942b72b823ee99d3ba7be43fb38d38c0364c0
giji676 <tvtengoxachidze@gmail.com>
2025-10-25 02:44:36 +0100

    invalid password test

--- backend/accounts/tests.py
+++ backend/accounts/tests.py
@@ -188,6 +188,11 @@ class LoginTest(APITestCase):
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
         self.assertIn("invalid credentials", response.data["error"].lower())
 
+    def test_invalid_password(self):
+        response = self.client.post(self.url, {"email": self.email, "password": "invalidpass"})
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertIn("invalid credentials", response.data["error"].lower())
+
 class RegisterTest(APITestCase):
     def setUp(self):
         self.url = reverse("register")
