19b84f527b0522d5a0e1fa65cfdd45d4a5aeeb94
giji676 <tvtengoxachidze@gmail.com>
2026-03-09 15:37:05 +0000

    Fixed DEBUG=TRUE in prod?

--- backend/movie/settings.py
+++ backend/movie/settings.py
@@ -37,7 +37,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG  = os.environ.get("DEBUG", False)
+DEBUG = os.environ.get("DEBUG", "False").lower() in ("true", "1", "yes")
 
 ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOST", "localhost").split(",")
 
