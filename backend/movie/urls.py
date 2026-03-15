8a391f916941328c108fa0ae15a84593612cd8e4
giji676 <tvtengoxachidze@gmail.com>
2026-03-15 01:07:11 +0000

    Added account admin page

--- backend/movie/urls.py
+++ backend/movie/urls.py
@@ -2,9 +2,10 @@ from django.urls import path, include
 from django.http import HttpResponse
 from django.conf import settings
 from django.conf.urls.static import static
-
+from django.contrib import admin
 
 urlpatterns = [
+    path("admin/", admin.site.urls),
     path("api/", include("api.urls")),
     path("api/user/", include("accounts.urls")),
     path("api/auth/", include("rest_framework.urls")),
